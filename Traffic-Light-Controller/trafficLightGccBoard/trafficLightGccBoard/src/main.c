56c7d1939b9329e7eb144d1d1f46b77d93cab90b
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2024-06-19 02:15:00 +0330

    display time on 4 sevenSegments

--- Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
+++ Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
@@ -18,18 +18,24 @@ uint16_t ADC_read(void) {
 	return ADC;
 }
 
-void show_time(uint8_t sec) {
-	uint8_t nums[] = {0x3F, 0x06, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x07, 0xFF, 0xEF};
+void show_time(uint8_t sec, uint8_t seg_index) {
+	uint8_t nums[] = {
+		0x3F, 0x06, 0xDB, 0xCF, 0xE6,
+		0xED, 0xFD, 0x07, 0xFF, 0xEF
+	};
+	
 	uint8_t n1 = sec % 10;
 	uint8_t n2 = sec / 10;
 	
 	PORTC = nums[n1];
-	PORTD = 0xFE;
-	_delay_ms(5);
+	PORTD = ~(1<<seg_index);
+	//_delay_ms(1);
+	_delay_us(600);
 	
 	PORTC = nums[n2];
-	PORTD = 0xFD;
-	_delay_ms(5);	
+	PORTD = ~(1<<(seg_index+1));
+	//_delay_ms(1);
+	_delay_us(600);	
 }
 
 int main (void) {
@@ -46,9 +52,9 @@ int main (void) {
 	uint16_t max = 60;
 		
 	while(1) {
-		//ADC_value = ADC_read();
-		//uint16_t green_time = (((uint32_t)ADC_value * (max - min)) / 1023) + min;
-		show_time(90);
+		show_time(12, 0);
+		show_time(34, 2);
+
 	}
 	
 }
