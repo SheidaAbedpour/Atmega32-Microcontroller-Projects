40f1c5c622c618f43edc503f57fcd4bb0386739d
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2024-06-19 01:21:30 +0330

    show digits debuge

--- Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
+++ Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
@@ -1,5 +1,5 @@
 #ifndef F_CPU
-#define F_CPU 8000000UL
+#define F_CPU 16000000UL
 #endif
 
 #include <avr/io.h>
@@ -19,20 +19,17 @@ uint16_t ADC_read(void) {
 }
 
 void show_time(uint8_t sec) {
-	uint8_t nums[] = {0x3F, 0x06, 0xA5, 0x8D, 0x19, 0x49, 0x41, 0x1F, 0x01, 0x09};
-	
+	uint8_t nums[] = {0x3F, 0x06, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x07, 0xFF, 0xEF};
 	uint8_t n1 = sec % 10;
 	uint8_t n2 = sec / 10;
 	
 	PORTC = nums[n1];
-	//PORTD = 0xEF;
-	PORTD = 0x01;
-	_delay_ms(500);
+	PORTD = 0xFE;
+	_delay_ms(5);
 	
 	PORTC = nums[n2];
-	//PORTD = 0xFD;
-	PORTD = 0x02;
-	_delay_ms(500);
+	PORTD = 0xFD;
+	_delay_ms(5);	
 }
 
 int main (void) {
@@ -42,15 +39,16 @@ int main (void) {
 	ADC_init();
 	
 	DDRC = 0xFF;
-	DDRD = 0x03;
+	DDRD = 0xFF;
 	
 	uint16_t ADC_value;
 	uint16_t min = 10;
 	uint16_t max = 60;
-	
+		
 	while(1) {
 		//ADC_value = ADC_read();
 		//uint16_t green_time = (((uint32_t)ADC_value * (max - min)) / 1023) + min;
-		show_time(10);
+		show_time(90);
 	}
+	
 }
