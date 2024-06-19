540273439f7df4ee831a0a79a1b0d8058c0e064d
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2024-06-19 19:02:51 +0330

    functions for controlling lights added

--- Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
+++ Traffic-Light-Controller/trafficLightGccBoard/trafficLightGccBoard/src/main.c
@@ -1,13 +1,18 @@
 #ifndef F_CPU
-#define F_CPU 16000000UL
+#define F_CPU 1000000UL
 #endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 
+uint8_t sec;
+enum {GREEN, YELLOW, RED};
+uint8_t light_state = GREEN;
+
+
 void ADC_init() {
-	ADCSRA = 0x87;
+	ADCSRA = 0x86;
 	ADMUX = 0x63;
 }
 
@@ -17,6 +22,7 @@ uint16_t ADC_read(void) {
 	ADCSRA |= (1<<ADIF);
 	return ADC;
 }
+
 
 void show_time(uint8_t sec, uint8_t seg_index) {
 	uint8_t nums[] = {
@@ -37,6 +43,40 @@ void show_time(uint8_t sec, uint8_t seg_index) {
 	//_delay_ms(1);
 	_delay_us(600);	
 }
+
+
+void update_traffic_lights(uint8_t state) {
+	switch (state) {
+		case GREEN:
+		case YELLOW:
+		case RED:
+	}	
+}
+
+void set_traffic_lights() {
+	switch (light_state) {
+		case GREEN:
+		case YELLOW:
+		case RED:
+	}
+}
+
+
+void init_timer0() {
+	DDRB = ;
+	TCNT0= 0x00;
+	OCR0 = ;
+	TCCR0 = ;
+	sei();
+}
+
+ISR(TIMER0_COMP_vect) {
+	if(sec > 0){
+		
+	}
+	update_traffic_lights();
+}
+
 
 int main (void) {
 	DDRA &= ~(1<<PA3);
